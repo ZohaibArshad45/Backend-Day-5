7267fb608f829913441306c0d645113b00d35e48
Zohaib Arshad <109464646+ZohaibArshad45@users.noreply.github.com>
2024-05-16 22:27:02 +0500

    Form Handling, Sessions & Cookies, with code

---
+++ script.js
@@ -0,0 +1,20 @@
+// 1, install express
+//  1, install cookie-parser
+
+const express = require('express')
+const { Session } = require('inspector')
+const app = express()
+
+
+
+// for Session and ccokies we need to use this two lines
+// this help us to 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
